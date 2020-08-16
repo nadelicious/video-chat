5db4c92aa52c1de69885a4955df29273fc1ef196
Ronald Gemao <ronald.gemao@wavecell.com>
2020-08-17 01:57:36 +0800

    refactor: minor changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mx-auto px-4">
     <h1 class="text-xl mb-10">Live Video Example</h1>
-    <a href="/agent">Agent </a>
-    <a href="/guest">Guest </a>
+    <a href="/agent" class="block mb-5">Connect as Agent </a>
+    <a href="/guest" class="block">Connect as Guest </a>
   </div>
 </template>
 
