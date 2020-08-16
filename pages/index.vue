895af864a5c0264bf77bb275e8fe8625e7470b90
Ronald Gemao <ronald.gemao@wavecell.com>
2020-08-17 01:52:36 +0800

    refactor: modify dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mx-auto px-4">
-    <h1 class="text-xl">Dashboard</h1>
+    <h1 class="text-xl mb-10">Live Video Example</h1>
+    <a href="/agent">Agent </a>
+    <a href="/guest">Guest </a>
   </div>
 </template>
 
