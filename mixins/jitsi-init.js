8f979dcc220dc5fda7fc28373ef1687c7efd8245
Ronald Gemao <ronald.gemao@wavecell.com>
2020-08-17 22:49:15 +0800

    chore: log the jitsi config

--- mixins/jitsi-init.js
+++ mixins/jitsi-init.js
@@ -12,6 +12,9 @@ export default {
       let config = jitsiConfig
 
       config = { ...config, ...options }
+
+      console.log('*** jitsi config ***', config)
+
       // eslint-disable-next-line no-undef
       this.jitsiApi = new JitsiMeetExternalAPI(domain, config)
     }
